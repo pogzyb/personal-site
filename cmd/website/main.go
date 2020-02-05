0e3b770a9e887336d2f893cbbfcfe3008030fb70
Joe <pogzyb@umich.edu>
2020-02-05 18:46:05 -0500

    Put main.go into cmd folder

--- cmd/website/main.go
+++ cmd/website/main.go
@@ -1,9 +1,16 @@
 package main
 
-func init() {
+import (
+	"github.com/pogzyb/website/web/app"
+	log "github.com/sirupsen/logrus"
+)
 
+func init() {
+	log.SetFormatter(&log.TextFormatter{})
 }
 
 func main() {
-
+	log.Info("Starting Application!")
+	application := app.New()
+	application.Start()
 }
