131b4cedd4e6f5f0873296f5a0fc9b6f3a9b92fb
Joe Obarzanek <pogzyb@umich.edu>
2020-11-11 14:28:13 -0500

    Add 500 and 404 handlers

--- website/handlers.go
+++ website/handlers.go
@@ -1,65 +1,97 @@
 package website
 
 import (
-	//"errors"
-	//"fmt"
+	"fmt"
+	"github.com/julienschmidt/httprouter"
+	"html/template"
 	"log"
 	"net/http"
-
-	"github.com/julienschmidt/httprouter"
+	"runtime/debug"
+	"time"
 )
 
 type (
+	Articles []Article
+	Projects []Project
+
 	Page struct {
 		Title		string
-		Content 	[]interface{}
+		Articles
+		Projects
 	}
-
-	Articles []Article
 )
 
+func NotFoundHandler(w http.ResponseWriter, r *http.Request) {
+	page := Page{ Title: "404 Error" }
+	notFoundTmpl := templates.Lookup("404.gohtml")
+	if err := notFoundTmpl.ExecuteTemplate(w, "404", page); err != nil {
+		log.Fatalf("Could not write response for '404' page: %v", err)
+	}
+}
+
+func InternalServerError(w http.ResponseWriter, r *http.Request, err interface{}) {
+	subject := fmt.Sprintf("500 Error [%s]", time.Now().Format(time.RFC822))
+	serverErr := err.(error)
+	stack := string(debug.Stack())
+	sendEmail(subject, serverErr, stack)
+	page := Page{ Title: "500 Error" }
+	internalServerErrorTmpl := templates.Lookup("500.gohtml")
+	if err := internalServerErrorTmpl.ExecuteTemplate(w, "500", page); err != nil {
+		log.Fatalf("Could not write response for '500' page: %v", err)
+	}
+}
+
 func (s Site) index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	indexTmpl := s.Templates.Lookup("index.gohtml")
-	if err := indexTmpl.ExecuteTemplate(w, "index", nil); err != nil {
+	indexTmpl := templates.Lookup("index.gohtml")
+	page := Page{ Title: "Home" }
+	if err := indexTmpl.ExecuteTemplate(w, "index", page); err != nil {
 		log.Fatalf("Could not write response for 'index' page: %v", err)
 	}
 }
 
 func (s Site) resume(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	resumeTmpl := s.Templates.Lookup("resume.gohtml")
-	if err := resumeTmpl.ExecuteTemplate(w, "resume", nil); err != nil {
+	resumeTmpl := templates.Lookup("resume.gohtml")
+	page := Page{ Title: "Resume" }
+	if err := resumeTmpl.ExecuteTemplate(w, "resume", page); err != nil {
 		log.Fatalf("could not write response for 'resume' page: %v", err)
 	}
 }
 
 func (s Site) projects(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	projectsTmpl := s.Templates.Lookup("projects.gohtml")
-	if err := projectsTmpl.ExecuteTemplate(w, "projects", nil); err != nil {
+	var allProjects Projects
+	queryResult := database.Find(&allProjects)
+	if queryResult.Error != nil {
+		log.Printf("could not query projects: %v", queryResult.Error)
+	}
+	projectsTmpl := templates.Lookup("projects.gohtml")
+	page := Page{ Title: "Projects", Projects: allProjects }
+	if err := projectsTmpl.ExecuteTemplate(w, "projects", page); err != nil {
 		log.Fatalf("could not write response for 'projects' page: %v", err)
 	}
 }
 
 func (s Site) articles(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	var many Articles
-	queryResult := database.Find(&many)
-	log.Println(queryResult.RowsAffected)
+	var allArticles Articles
+	queryResult := database.Find(&allArticles)
 	if queryResult.Error != nil {
 		log.Printf("could not query articles: %v", queryResult.Error)
 	}
-	articlesTmpl := s.Templates.Lookup("articles.gohtml")
-	if err := articlesTmpl.ExecuteTemplate(w, "articles", many); err != nil {
+	articlesTmpl := templates.Lookup("articles.gohtml")
+	page := Page{ Title: "Articles", Articles: allArticles }
+	if err := articlesTmpl.ExecuteTemplate(w, "articles", page); err != nil {
 		log.Fatalf("could not write response for 'articles' page: %v", err)
 	}
 }
 
-//func (s Site) article(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-//	link := ps.ByName("link")
-//	a, err := getArticleByLink(link)
-//	if err != nil {
-//		log.Fatal(err)
-//	}
-//	article := s.Templates.Lookup(a.TmplName)
-//	if err := article.ExecuteTemplate(w, a.TmplName, nil); err != nil {
-//		log.Fatalf("Could not write response for 'resume' page... %s", err.Error())
-//	}
-//}
+func (s Site) article(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	var oneArticle Article
+	path := ps.ByName("articlePath")
+	queryResult := database.Where("path = ?", path).First(&oneArticle)
+	if queryResult.Error != nil {
+		log.Printf("could not query article: %s: %v", path, queryResult.Error)
+	}
+	articleTmpl := templates.Lookup(oneArticle.HTMLTemplate)
+	if err := articleTmpl.ExecuteTemplate(w, oneArticle.Template, oneArticle); err != nil {
+		log.Fatalf("Could not write response for 'article' %s: %v", oneArticle.Title, err)
+	}
+}
--- website/server.go
+++ website/server.go
@@ -3,7 +3,6 @@ package website
 import (
 	"github.com/julienschmidt/httprouter"
 	"net/http"
-
 	"html/template"
 	"io/ioutil"
 	"log"
@@ -11,33 +10,38 @@ import (
 	"path"
 )
 
+var templates *template.Template
+
 type Site struct {
 	Router    *httprouter.Router
-	Templates *template.Template
+}
+
+func init() {
+	initDatabase()
+	initSession()
+	initTemplates()
+	migrateDatabase()
 }
 
 func NewSite() *Site {
-	site := Site{
-		Router:    initRouter(),
-		Templates: initTemplates(),
-	}
+	site := Site{Router: initRouter()}
 	site.Router.GET("/", site.index)
 	site.Router.GET("/resume", site.resume)
 	site.Router.GET("/projects", site.projects)
 	site.Router.GET("/articles", site.articles)
-	//site.Router.GET("/article/:link", site.article)
+	site.Router.GET("/article/:articlePath", site.article)
 	return &site
 }
 
 func initRouter() *httprouter.Router {
 	router := httprouter.New()
-	// add handlers
-	//router.GET("/", Site.index)
 	router.ServeFiles("/static/*filepath", http.Dir("static"))
+	router.NotFound = http.HandlerFunc(NotFoundHandler)
+	router.PanicHandler = InternalServerError
 	return router
 }
 
-func initTemplates() *template.Template {
+func initTemplates() {
 	var templateFiles []string
 	templatesDir := os.Getenv("TEMPLATES_DIR")
 	// read and parse template files
@@ -48,9 +52,8 @@ func initTemplates() *template.Template {
 	for _, file := range files {
 		templateFiles = append(templateFiles, path.Join(templatesDir, file.Name()))
 	}
-	templates, err := template.ParseFiles(templateFiles...)
+	templates, err = template.ParseFiles(templateFiles...)
 	if err != nil {
 		log.Fatalf("could not parse html templates: %v", err)
 	}
-	return templates
 }
